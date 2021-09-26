2a65dd05b85e21ea33dce2826be2ea9612b3b27a
Chandima Ranaweera <chan4lk@gmail.com>
2021-09-26 15:49:49 +0530

    move state

---
+++ src/components/Todo.state.js
@@ -0,0 +1,35 @@
+import { createStore } from "solid-js/store";
+
+export const useStore = () => {
+    const [state, setState] = createStore({
+        value: '',
+        todos: []
+    });
+
+    const addTodo = () => {
+        setState(
+            {
+                todos: [
+                    ...state.todos,
+                    { title: state.value, completed: false }
+                ],
+                value: ''
+            })
+    }
+
+    const onChange = (e) => setState('value', e.target.value);
+
+    const toggleCompleted = todo => {
+        const todos = [...state.todos];
+        const index = todos.findIndex(t => t.title === todo.title);
+
+        setState('todos', [index], 'completed', c => !c);
+    }
+
+    return {
+        state,
+        addTodo,
+        onChange,
+        toggleCompleted
+    }
+}
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,31 +1,10 @@
-import { createStore } from "solid-js/store";
+
 import { Todo } from "./Todo";
+import { useStore } from "./Todo.state";
 
 export function TodoList() {
-    const [state, setState] = createStore({
-        value: '',
-        todos: []
-    });
-
-    const addTodo = () => {
-        setState(
-            {
-                todos: [
-                    ...state.todos,
-                    { title: state.value, completed: false }
-                ],
-                value: ''
-            })
-    }
-
-    const onChange = (e) => setState('value', e.target.value);
-
-    const toggleCompleted = todo => {
-        const todos = [...state.todos];
-        const index = todos.findIndex(t => t.title === todo.title);
-
-        setState('todos', [index], 'completed', c => !c);
-    }
+    
+    const { state, addTodo, onChange, toggleCompleted } = useStore();
 
     return <div>
         <h1>Todo App</h1>
